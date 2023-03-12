996374c15018980d62c0ea17469d4a4e6016c985
Murega14 <tedmurega@gmail.com>
2023-03-12 14:19:41 +0300

    first commit

---
+++ app/src/main/java/com/example/finnext/Login_Activity.java
@@ -0,0 +1,92 @@
+package com.example.finnext;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText emailEditText, passwordEditText;
+    private Button loginButton;
+    private TextView registerTextView;
+
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.LoginActivity);
+
+        auth = FirebaseAuth.getInstance();
+
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        loginButton = findViewById(R.id.loginButton);
+        registerTextView = findViewById(R.id.registerTextView);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString().trim();
+
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            private void handleSignInResult(Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // User signed in successfully
+                                    FirebaseAuth mAuth = null;
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    // TODO: Update UI with user information
+                                } else {
+                                    // Sign in failed, display a message to the user
+                                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            }
+
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    FirebaseUser user = auth.getCurrentUser();
+                                    Toast.makeText(LoginActivity.this, "Logged in as " + user.getEmail(), Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(LoginActivity.this, LoginActivity.class));
+                                    finish();
+                                } else {
+                                    Toast.makeText(LoginActivity.this, "Authentication failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+        registerTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
---
+++ app/src/main/java/com/example/finnext/Register_Activity.java
@@ -0,0 +1,4 @@
+package com.example.finnext;
+
+public class Register_Activity {
+}
