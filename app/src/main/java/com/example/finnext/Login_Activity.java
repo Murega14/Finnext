4a560b2e1d4e3b2ae35f53420c52a6642759620e
Murega14 <tedmurega@gmail.com>
2023-03-14 09:53:40 +0300

    first commit

--- app/src/main/java/com/example/finnext/Login_Activity.java
+++ app/src/main/java/com/example/finnext/Login_Activity.java
@@ -89,4 +89,8 @@ public class LoginActivity extends AppCompatActivity {
         registerTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class))
+            }
+
+            ;
+        }
